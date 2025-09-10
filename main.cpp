052a30549e3d572fa6c6fdcd4815768b271d620b
AndriusKovalevskij <andrius.kovalevskij@gmail.com>
2025-09-10 09:28:13 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using std::vector;
 using std::setw;
 using std::left;
 using std::right;
+using std::fixed;
 
 struct Studentas
 {
@@ -23,29 +24,78 @@ struct Studentas
 
 };
 
-Studentas Stud_ivestis()
+// Funkcija pazymiu ivesties klaidoms gaudyti
+int ivestiSkaiciu(const string& zinute, int min_pazymys = 1, int max_pazymys = 10)
+{
+    int skaicius;
+    cout << zinute;
+    if(!(cin >> skaicius))
+    {
+        cout << "Klaida: ivestis turi buti skaicius!" << endl;
+        exit(1);
+    }
+    if(skaicius < min_pazymys || skaicius > max_pazymys)
+    {
+        cout << "Klaida: skaicius turi buti nuo " << min_pazymys << " iki " << max_pazymys << "!" <<endl;
+        exit(1);
+    }
+    return skaicius;
+}
+
+// Funkcija vardo/pavardes ivesties klaidos gaudyti
+string ivestiVarda(const string& zinute)
+{
+    string vardas;
+    cout << zinute;
+    cin >> vardas;
+
+    for (char c : vardas)
+    {
+        if (!isalpha(c))
+        {
+            cout << "Klaida: vardas/pavarde turi buti tik is raidziu!" << endl;
+            exit(1);
+        }
+    }
+
+    if (vardas.empty())
+    {
+        cout << "Klaida: vardas/pavarde negali buti be raidziu!" << endl;
+        exit(1);
+    }
+    return vardas;
+}
+
+// Studento duomenu ivestis
+Studentas Stud_ivestis(int studentoNr)
 {
     int n, laikinas_paz, sum = 0;
     Studentas pirmas;
-    cout << "Iveskite duomenis" << endl;
-    cout << "Vardas: ";
-    cin >> pirmas.vardas;
-    cout << "Pavarde: ";
-    cin >> pirmas.pavarde;
+    cout << "\nIveskite " << studentoNr << "-o studento duomenis" << endl;
+    pirmas.vardas = ivestiVarda("Vardas: ");
+    pirmas.pavarde = ivestiVarda("Pavarde: ");
     cout << "Kiek namu darbu pazymiu turi " << pirmas.vardas << " " << pirmas.pavarde << "? ";
-    cin >> n;
+    n = ivestiSkaiciu("", 0, 10);
 
-    for (int a = 0; a < n; a++)
-    {
-        cout << a + 1 << ": ";
-        cin >> laikinas_paz;
+    if (n == 0) {
+        cout << "Studentas neturi namu darbu pazymiu." << endl;
+    } else {
+        cout << "Iveskite namu darbu pazymius (1-10):" << endl;
+        for (int a = 0; a < n; a++)
+        {
+        laikinas_paz =ivestiSkaiciu(to_string(a + 1) + ": ");
         pirmas.ndpazymiai.push_back(laikinas_paz);
         sum += laikinas_paz;
+        }
     }
+    pirmas.egzrezultatas = ivestiSkaiciu("Iveskite egzamino rezultata (1-10): ");
+    if (n > 0)
+    {
+        pirmas.galutinis = double(sum) / double(n) * 0.4 + pirmas.egzrezultatas * 0.6;
 
-    cout << "Iveskite egzamino rezultata: ";
-    cin >> pirmas.egzrezultatas;
-    pirmas.galutinis = double(sum) / double(n) * 0.4 + pirmas.egzrezultatas * 0.6;
+    } else {
+        pirmas.galutinis = pirmas.egzrezultatas * 0.6;
+    }
     return pirmas;
 }
 
@@ -53,16 +103,37 @@ int main()
 {
     cout << "Laba diena" << endl;
     vector<Studentas> Grupe;
-    cout << "Kiek studentu grupeje?: ";
-    int m;
-    cin >> m;
+    int m = ivestiSkaiciu("Kiek studentu grupeje?: ", 1, 100000);
 
     for (auto z = 0; z < m; z++)
     {
-        Grupe.push_back(Stud_ivestis());
+        Grupe.push_back(Stud_ivestis(z + 1));
     }
 
     cout << "\nStudento informacija:" << endl;
+    cout << left << setw(10) << "Vardas" << "|"
+         << left << setw(15) << "Pavarde" << "|"
+         << left << setw(20) << "ND pazymiai" << "|"
+         << left << setw(10) << "Galutinis (Vid.)" << endl;
+    cout << string(70, '-') << endl;
 
+    for (auto Past : Grupe)
+    {
+        cout << setw(10) << left << Past.vardas << "|"
+             << setw(15) << left << Past.pavarde << "|";
+
+        string pazymiai_str = "";
+        if (Past.ndpazymiai.empty())
+        {
+            pazymiai_str = "nera";
+        } else {
+            for (auto& a : Past.ndpazymiai)
+            {
+                pazymiai_str += to_string(a) + " ";
+            }
+        }
+        cout << setw(20) << left << pazymiai_str << "|"
+             << fixed << setprecision(2) << setw(10) << left << Past.galutinis << endl;
+    }
     return 0;
 }
